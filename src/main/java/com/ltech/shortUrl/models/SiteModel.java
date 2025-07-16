fb6d1a58d84ab3d614873086c5b6570871902864
lucasbennertz <lucasgcbennertz@gmail.com>
2025-07-16 19:59:34 -0300

    qrcode em base64

--- src/main/java/com/ltech/shortUrl/models/SiteModel.java
+++ src/main/java/com/ltech/shortUrl/models/SiteModel.java
@@ -1,8 +1,19 @@
 package com.ltech.shortUrl.models;
 
+import java.util.Hashtable;
 import java.util.Random;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import javax.imageio.ImageIO;
+import java.util.Base64;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -22,21 +33,59 @@ public class SiteModel {
     Long ID;
     String urlOriginal;
     String urlEncurtada;
-    BitMatrix qrCode;
+    String qrCodeBase64;
 
-    SiteModel(String urlOriginal){
+    public SiteModel(String urlOriginal){
         this.urlOriginal = urlOriginal;
         this.urlEncurtada = EncurtarUrl();
+        BitMatrix matrix = createQRImage(urlOriginal);
+        this.qrCodeBase64 = bitMatrixToBase64(matrix);
     };
+
+    public SiteModel(){}
     
     private String EncurtarUrl(){
         String pattern = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         Random generator = new Random();
-        StringBuffer senha = new StringBuffer();
+        StringBuilder senha = new StringBuilder();
         for(int i = 0; i < 5; i++){
             int valor = generator.nextInt(52);
             senha.append(pattern.charAt(valor));
         }
         return senha.toString();
     }
+    private BitMatrix createQRImage(String qrCodeText ){
+        BitMatrix byteMatrix = null;
+		try{
+		Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<>();
+		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+		QRCodeWriter qrCodeWriter = new QRCodeWriter();
+		byteMatrix = qrCodeWriter.encode(qrCodeText, BarcodeFormat.QR_CODE, 100, 100, hintMap);
+        }catch(WriterException e){
+            System.out.println("Deu erro aqui");
+            e.printStackTrace();
+        }
+        return byteMatrix;
+	}
+    private String bitMatrixToBase64(BitMatrix matrix) {
+    try {
+        int width = matrix.getWidth();
+        int height = matrix.getHeight();
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setRGB(x, y, matrix.get(x, y) ? 0x000000 : 0xFFFFFF);
+            }
+        }
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(image, "png", baos);
+        return Base64.getEncoder().encodeToString(baos.toByteArray());
+    } catch (Exception e) {
+        e.printStackTrace();
+        return null;
+    }
 }
+
+}
+
