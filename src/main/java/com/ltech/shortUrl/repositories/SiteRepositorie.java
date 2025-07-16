412f5412f8f8c2f2d90e58fc21a3844323fc996f
lucasbennertz <lucasgcbennertz@gmail.com>
2025-07-16 20:29:47 -0300

    criação do service

--- src/main/java/com/ltech/shortUrl/repositories/SiteRepositorie.java
+++ src/main/java/com/ltech/shortUrl/repositories/SiteRepositorie.java
@@ -1,7 +1,13 @@
 package com.ltech.shortUrl.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface SiteRepositorie extends JpaRepository<String, Long>{
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ltech.shortUrl.models.SiteModel;
+
+@Repository
+public interface SiteRepositorie extends JpaRepository<SiteModel, Long>{
+    Optional<SiteModel> findByUrlOriginal(String urlEncurtada);
 }
---
+++ src/main/java/com/ltech/shortUrl/services/SiteService.java
@@ -0,0 +1,28 @@
+package com.ltech.shortUrl.services;
+
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ltech.shortUrl.models.SiteModel;
+import com.ltech.shortUrl.repositories.SiteRepositorie;
+
+@Service
+public class SiteService {
+    
+    private final SiteRepositorie repositorie;
+
+    public SiteService(SiteRepositorie repositorie){
+        this.repositorie = repositorie;
+    }
+
+    public List<SiteModel> getAll(){
+        return this.repositorie.findAll();
+    }
+    public String getUrlOriginal(String shortUrl){
+        return repositorie.findByUrlOriginal(shortUrl)
+        .map(SiteModel:: getUrlOriginal)
+        .orElse(null);
+    }
+}
