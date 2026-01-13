ebb4324803d9328f7c1f4650d9598045a431bbae
lucasbennertz <lucasgcbennertz@gmail.com>
2026-01-12 22:13:49 -0300

    conversão dto para model

--- src/main/java/com/ltech/shortUrl/models/UserModel.java
+++ src/main/java/com/ltech/shortUrl/models/UserModel.java
@@ -1,30 +1,4 @@
 package com.ltech.shortUrl.models;
 
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "user")
 public class UserModel {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(name = "username", nullable = false)
-    private String username;
-    @Column(name = "password", nullable = false)
-    private String password;
-    @Column(name = "email", nullable = false)
-    private String email;
-    
-    public UserModel(String username, String password, String email) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
 }
